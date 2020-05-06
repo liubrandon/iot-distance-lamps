246f1f9d4a0aa85111f7fcdd22dd23369c18db07
liubrandon <byliu88@gmail.com>
2020-05-06 12:04:38 -0400

    Added push button to change color

--- src/iot-distance-lamps.ino
+++ src/iot-distance-lamps.ino
@@ -15,9 +15,6 @@
 #define PIXEL_COUNT 24
 #define PIXEL_TYPE WS2812B
 Adafruit_NeoPixel strip(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);
-uint32_t Wheel(uint8_t position);
-void fill(Adafruit_NeoPixel* strip, uint32_t color);
-
 #define NUM_COLORS 6
 int currColor = 0;
 unsigned long colorPalette[NUM_COLORS] = {
@@ -34,73 +31,53 @@ unsigned long colorPalette[NUM_COLORS] = {
   strip.Color(41, 128, 185),
   strip.Color(255, 195, 0),
 };
+// Prototypes for functions used below
+uint32_t Wheel(uint8_t position);
+void fill(Adafruit_NeoPixel* strip, uint32_t color);
 
-String touchEventName = "touch_event";
-// bool pressing = false;
-// unsigned int lastPress, lastRelease, previousLastPress, previousLastRelease;
-//
-// int buttonHandler()  // 0 = not pressing, 1 = still pressing, 2 = just stopped pressing, 3 = just started pressing
-// {
-//         if (digitalRead(BUTTON) == LOW && pressing == false) // 3 = just started pressing
-//         {
-//                 pressing = true;
-//                 lastPress = millis();
-//                 return 3;
-//         }
-//
-//         if (digitalRead(BUTTON) == HIGH && pressing == true) // 2 = just stopped pressing
-//         {
-//                 pressing = false;
-//                 lastRelease = millis();
-//                 return 2;
-//         }
-//
-//         if (pressing == true) // 1 = still pressing
-//         {
-//                 return 1;
-//         }
-//         else // 0 = not pressing
-//         {
-//                 return 0;
-//         }
-// }
+// Globals and code to handle button press
+String changeEventName = "change_event";
+bool pressing = false;
+unsigned int lastPress, lastRelease, previousLastPress, previousLastRelease;
+int buttonHandler() { // 0 = not pressing, 1 = still pressing, 2 = just stopped pressing, 3 = just started pressing
+    if (digitalRead(BUTTON) == LOW && pressing == false) { // 3 = just started pressing
+        pressing = true;
+        lastPress = millis();
+        return 3;
+    }
+    if (digitalRead(BUTTON) == HIGH && pressing == true) { // 2 = just stopped pressing
+        pressing = false;
+        lastRelease = millis();
+        return 2;
+    }
+    if (pressing == true) { // 1 = still pressing
+        return 1;
+    }
+    else { // 0 = not pressing
+        return 0;
+    }
+}
 
 void setup() {
+  // initializes Particle cloud functions
   Particle.function("next-color", nextColor);
   Particle.function("set-brightness", mySetBrightness);
-  // set up passive pull-up,
-  // when button is pressed BUTTON_PIN will read LOW since it's connected to ground
-  pinMode(BUTTON, INPUT_PULLUP);
-  digitalWrite(BUTTON, HIGH);
+  // set up passive pull-up
+  pinMode(BUTTON, INPUT_PULLUP); // when button is pressed BUTTON_PIN will read LOW since it's connected to ground
   Serial.begin(9600);
-
-
   strip.begin();
   strip.setBrightness(BRIGHTNESS_LEVEL);
-  strip.show();
   fill(&strip, colorPalette[currColor]);
   strip.show();
 }
 
-// // Variables will change:
-// int ledState = HIGH;         // the current state of the output pin
-// int buttonState;             // the current reading from the input pin
-// int lastButtonState = LOW;   // the previous reading from the input pin
-// unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
-// unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
 void loop() {
-  // int check = buttonHandler(); // get press status
-  // if (previousLastRelease != lastRelease) // Button bumped, pushed and then released at later time
-  // {
-  //         Particle.publish("click","Click took "+String(lastRelease - lastPress)+" ms");
-  //         nextColor();
-  //         previousLastRelease = lastRelease;
-  // }
-  // int buttonState = digitalRead(BUTTON_PIN);
-  // if(buttonState == HIGH) { // button pressed
-  //   Serial.println("Is high");
-  //   nextColor();
-  // }
+  int check = buttonHandler(); // get press status
+  if (previousLastRelease != lastRelease) { // Button bumped, pushed and then released at later time
+      Particle.publish("click","Click took "+String(lastRelease - lastPress)+" ms");
+      nextColor("");
+      previousLastRelease = lastRelease;
+  }
 }
 
 int nextColor(String extra) {
@@ -122,7 +99,7 @@ int mySetBrightness(String text) {
 // Smoothly transition between two colors by calculating
 // the 3D vector between their RGB values and traversing it
 void transition(Adafruit_NeoPixel* strip, uint32_t startingColor, uint32_t endingColor) {
-  // Starting RGB values
+  // Starting RGB values (NeoPixel color 32 bits format: WWWWWWWW RRRRRRRR GGGGGGGG BBBBBBBB)
   uint8_t sr = startingColor >> 16;
   uint8_t sg = startingColor >> 8;
   uint8_t sb = startingColor;
@@ -141,19 +118,6 @@ void transition(Adafruit_NeoPixel* strip, uint32_t startingColor, uint32_t endin
     fill(strip, strip->Color(sr, sg, sb));
     strip->show();
   }
-
-  // Delta RGB values ("slope")
-  // int steps = 50;
-  // double dr = (sr-er)/steps;
-  // double dg = (sg-eg)/steps;
-  // double db = (sb-eb)/steps;
-  // uint32_t currColor = startingColor;
-  // for(int i = 0; i < steps; i++) {
-  //   fill(strip, currColor);
-  //   strip->show();
-  //   currColor = strip->Color((uint8_t)(sr+=dr), (uint8_t)(sg+=dg), (uint8_t)(sb+=db));
-  //   delay(10);
-  // }
 }
 
 // Set all pixels to color
